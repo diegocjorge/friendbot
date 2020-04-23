55315956aa9792a93787fab63f927052977c8c48
Diego Cuman Jorge <diegocj@outlook.com>
2020-04-23 16:56:35 -0300

    v0.4

--- FASE 2 - Jogo Asteroides/script.js
+++ FASE 2 - Jogo Asteroides/script.js
@@ -3,14 +3,14 @@ var ctx = canvas.getContext("2d");
 
 // CARREGAR TODAS AS IMAGENS E SONS
 var midia = {
+	esquerda: "imagens/e_nave.png",
 	cima: "imagens/c_nave.png",
 	direita: "imagens/d_nave.png",
 	baixo: "imagens/b_nave.png",
-	esquerda: "imagens/e_nave.png",
 	asteroide: "imagens/asteroide.png",
 	meteoro: "imagens/meteoro.png",
 };
-for (i in midia) {
+for (var i in midia) {
 	load = new Image();
 	load.src = midia[i];
 	load.onload = console.log("Arquivo '" + midia[i] + "' carregado.");
@@ -24,37 +24,64 @@ var meteo1 = new objeto(400, 20, 64, 5, "meteo");
 var meteo2 = new objeto(400, -44, 64, 5, "meteo");
 var meteo3 = new objeto(700, -44, 64, 5, "meteo");
 
-// GAMELOOP
+// GAME START
 requestAnimationFrame(gameloop);
 
+// MOVIMENTAÇÃO NAVE
+window.onkeydown = function comando(tecla) {
+	if (tecla.keyCode == 37) {
+		nave.img.src = midia.esquerda;
+		nave.x -= nave.vel;
+		if (nave.x < 0) {
+			nave.x = 0;
+		}
+	} else if (tecla.keyCode == 38) {
+		nave.img.src = midia.cima;
+		nave.y -= nave.vel;
+		if (nave.y < 0) {
+			nave.y = 0;
+		}
+	} else if (tecla.keyCode == 39) {
+		nave.img.src = midia.direita;
+		nave.x += nave.vel;
+		if (nave.x > 800 - nave.w) {
+			nave.x = 800 - nave.w;
+		}
+	} else if (tecla.keyCode == 40) {
+		nave.img.src = midia.baixo;
+		nave.y += nave.vel;
+		if (nave.y > 400 - nave.w) {
+			nave.y = 400 - nave.w;
+		}
+	}
+};
+
+// FUNÇÃO DE COLISÃO
+
+// FUNÇÃO DO PLACAR
+
+// FUNÇÃO AUMENTAR DIFICULDADE
+
+// GAMELOOP
+
 function gameloop() {
+	aster1.mover();
 	nave.desenhar();
 	aster1.desenhar();
 	aster2.desenhar();
 	meteo1.desenhar();
 	meteo2.desenhar();
 	meteo3.desenhar();
+	requestAnimationFrame(gameloop);
 }
 
-// COMANDOS DO TECLADO
-
-
-// FUNÇÃO DE COLISÃO
-
-
-// FUNÇÃO DO PLACAR
-
-
-// FUNÇÃO AUMENTAR DIFICULDADE
-
-
 // FUNÇÃO OBJETOS
-function objeto(x, y, wh, v, tipo) {
+function objeto(x, y, tamanho, velocidade, tipo) {
 	this.x = x;
 	this.y = y;
-	this.w = wh;
-	this.h = wh;
-	this.v = v;
+	this.w = tamanho;
+	this.h = tamanho;
+	this.vel = velocidade;
 	this.tipo = tipo; // Tipos: nave, aster, meteo
 	if (tipo == "nave") {
 		this.img = new Image();
@@ -71,4 +98,9 @@ function objeto(x, y, wh, v, tipo) {
 	this.desenhar = function () {
 		ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
 	};
+	this.mover = function() {
+		if (tipo == "aster") {
+
+		}
+	}
 }
